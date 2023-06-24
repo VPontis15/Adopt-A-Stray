918a6c5cd863c221187d08f81de3271133b7bc48
VPontis15 <94546957+VPontis15@users.noreply.github.com>
2023-06-24 19:37:11 +0300

    add mobile nav button fuction

--- js/pets.js
+++ js/pets.js
@@ -3,7 +3,7 @@ const navBar = document.querySelector(".main__nav");
 const navUL = document.querySelector(".nav-ul");
 const hamburgerIcon = document.querySelector(".mobile__nav-btn--icon");
 
-const revealMobileNav = function () {
+const revealMobileNav = function (e) {
   navBar.classList.toggle("hidden");
   navBar.classList.contains("hidden")
     ? (hamburgerIcon.src = "../images/burger-menu-svgrepo-com.svg")
